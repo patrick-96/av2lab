c7b116962d7bbee9ec3be947478f72df5343ecc0
Patrick <patrick.alvares96@gmail.com>
2017-03-30 00:06:45 -0300

    1.2

--- Av1/src/persistence/JuradoDao.java
+++ Av1/src/persistence/JuradoDao.java
@@ -1,5 +1,16 @@
 package persistence;
 
-public class JuradoDao {
+import java.sql.SQLException;
+import java.util.List;
+
+import model.Jurado;
+
+public class JuradoDao implements IJuradoDao{
+
+	@Override
+	public List<Jurado> consultaJurados() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
