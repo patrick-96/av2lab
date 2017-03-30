534ea155e834aa0928f270acf23eed0d67125044
Patrick <patrick.alvares96@gmail.com>
2017-03-30 00:22:30 -0300

    1.3

---
+++ Av1/src/persistence/EscolaDao.java
@@ -0,0 +1,16 @@
+package persistence;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import model.Escola;
+
+public class EscolaDao implements IEscolaDao{
+
+	@Override
+	public List<Escola> consultaEscolas() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
