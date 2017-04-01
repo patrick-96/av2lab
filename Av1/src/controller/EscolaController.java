07bc45f909148dd21f7274180e773910074269d2
Patrick <patrick.alvares96@gmail.com>
2017-04-01 19:22:01 -0300

    1.1.7

--- Av1/src/controller/EscolaController.java
+++ Av1/src/controller/EscolaController.java
@@ -1,9 +1,38 @@
 package controller;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
+
+import model.Escola;
+import persistence.EscolaDao;
+
 public class EscolaController implements IEscolaController{
+	private JComboBox<Escola> comboBox_e;
+	
+	public EscolaController(JComboBox<Escola> comboBox_e){
+		this.comboBox_e=comboBox_e;
+	}
 
 	public void listaEscola() {
-		// TODO Auto-generated method stub
+		try {
+			EscolaDao eDao = new EscolaDao();
+			List<Escola> listaEscola =eDao.consultaEscolas();
+			if(comboBox_e.getItemCount()>0){
+				comboBox_e.removeAllItems();
+			}
+			if(listaEscola!= null){
+				for(Escola e: listaEscola){
+					comboBox_e.addItem(e);
+				}
+			}
+		} catch (ClassNotFoundException | SQLException e) {
+			JOptionPane.showMessageDialog(null,e.getMessage(),"ERRO",JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
+		}
+		
 		
 	}
 
--- Av1/src/controller/JuradoControlller.java
+++ Av1/src/controller/JuradoControlller.java
@@ -1,10 +1,39 @@
 package controller;
 
-public class JuradoControlller implements IJuradoController{
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
+
+import model.Escola;
+import model.Jurado;
+import persistence.JuradoDao;
+
+public class JuradoControlller<comboBox_j> implements IJuradoController{
+	private JComboBox<Jurado> comboBox_j;
+	
+	public JuradoControlller(JComboBox<Jurado> comboBox_j){
+		this.comboBox_j=comboBox_j;
+	}
 
 	@Override
 	public void listaJurado() {
-		// TODO Auto-generated method stub
+		try {
+			JuradoDao jDao = new JuradoDao();
+			List<Jurado> listaJurado =jDao.consultaJurados();
+			if(comboBox_j.getItemCount()>0){
+				comboBox_j.removeAllItems();
+			}
+			if(listaJurado!= null){
+				for(Jurado j: listaJurado){
+					comboBox_j.addItem(j);
+				}
+			}
+		} catch (ClassNotFoundException | SQLException e) {
+			JOptionPane.showMessageDialog(null,e.getMessage(),"ERRO",JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
+		}
 		
 	}
 
