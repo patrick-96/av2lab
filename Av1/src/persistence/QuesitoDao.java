970363f34080a40430c0e2767272eade5cde1543
Patrick <patrick.alvares96@gmail.com>
2017-04-01 14:18:36 -0300

    com list do quesito feito

--- Av1/src/persistence/QuesitoDao.java
+++ Av1/src/persistence/QuesitoDao.java
@@ -1,16 +1,38 @@
 package persistence;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import model.Quesito;
 
 public class QuesitoDao implements IQuesitoDao{
-
+private Connection c;
+public QuesitoDao() throws ClassNotFoundException, SQLException{
+	IGenericDao gDao=new GenericDao();
+	c = gDao.getConnection();
+}
+	
 	@Override
 	public List<Quesito> consultaQuesitos() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		List<Quesito> listaQuesito=new ArrayList<Quesito>();
+		String sql="Select id_q,nome_q from Quesito";
+		PreparedStatement ps=c.prepareStatement(sql);
+		ResultSet rs=ps.executeQuery();
+		
+		while(rs.next()){
+			Quesito que= new Quesito();
+			que.setId_q(rs.getInt("id_q"));
+			que.setNome_q(rs.getString("nome_q"));
+			listaQuesito.add(que);
+			
+		}
+		rs.close();
+		ps.close();
+		return listaQuesito;
 	}
 
 }
